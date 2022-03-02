707928d033be6dd04d84d3c08c1553a6e3d338ec
MarcoSorci <97511096+MarcoSorci@users.noreply.github.com>
2022-03-02 13:16:04 +0100

    angley angles

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # FirstAngularApp
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.2.5.
+This project was generated wiiiiiiiiiiiith [Angular CLI](https://github.com/angular/angular-cli) version 13.2.5.
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PippoComponent } from './pippo/pippo.component';
-
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { WrapperComponent } from './components/wrapper/wrapper.component';
+import { ToDoListComponent } from './components/to-do-list/to-do-list.component';
+import { DoneListComponent } from './components/done-list/done-list.component';
+import { TaskInputComponent } from './components/task-input/task-input.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { StatisticsComponent } from './statistics/statistics.component';
 @NgModule({
-  declarations: [
-    AppComponent,
-    PippoComponent
+  declarations: [          //declared components
+    AppComponent, HeaderComponent, FooterComponent, WrapperComponent, ToDoListComponent, DoneListComponent, TaskInputComponent, FilterComponent, StatisticsComponent
   ],
-  imports: [
+  imports: [                //declared requirements
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent]  //the starting component
 })
 export class AppModule { }
